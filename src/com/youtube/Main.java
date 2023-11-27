14ba8cbe81fcfe03de78add5d24964fd722f2a0a
siriwardhanage <siriwardhanagerashmi@gmail.com>
2023-11-27 11:40:23 +0530

    create calculateMortgage method, *include all data to calculate the mortgage in this method

--- src/com/youtube/Main.java
+++ src/com/youtube/Main.java
@@ -9,12 +9,11 @@ public class Main {
         System.out.println("****** Mortgage Calculator ***** \n");
 
         Scanner scanner = new Scanner(System.in);
-        final int MONTHS_IN_YEARS = 12;
-        final int PERCENT = 100;
+
 
         double principal = 0;
-        double monthlyInterest = 0;
-        double numberOfPayment = 0;
+        double annualRate = 0;
+        int year = 0;
 
         //for principal
         while (true) {
@@ -28,36 +27,49 @@ public class Main {
         //for Annual Interest Rate
         while (true){
             System.out.print("Annual Interest Rate : ");
-            double annualRate = scanner.nextDouble();
-            if (annualRate >= 1 && annualRate <= 30) {
-                //for monthly Interest Rate
-                monthlyInterest = annualRate / MONTHS_IN_YEARS / PERCENT;
+            annualRate = scanner.nextDouble();
+            if (annualRate >= 1 && annualRate <= 30)
                 break;
-            }
             System.out.println("Enter a value between 1 and 30");
         }
 
         //for time period
         while (true){
             System.out.print("Period (Years) : ");
-            double year = scanner.nextDouble();
-            //No of payment
-            if (year >= 1 && year <= 30) {
-                numberOfPayment = year * MONTHS_IN_YEARS;
+            year = scanner.nextInt();
+            if (year >= 1 && year <= 30)
                 break;
-            }
             System.out.println("Enter a value between 1 and 30");
         }
 
-        //for monthly payment
-         /*          M = P * r(1+r)^n / [(1+r)^n - 1]      */
-        //for (1+r)^n
-       double mortgage = principal
-               * monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayment)
-               / (Math.pow(1 + monthlyInterest, numberOfPayment)-1);
+        double mortgage = calculateMortgage(principal, annualRate, year);
 
        String monthlyPayment = NumberFormat.getCurrencyInstance(new Locale("en","LK")).format(mortgage);
        System.out.println("Monthly Payment : " + monthlyPayment);
 
+    }
+
+    public static double calculateMortgage(
+            double principal,
+            double annualRate,
+            int year){
+
+        final int MONTHS_IN_YEARS = 12;
+        final int PERCENT = 100;
+
+        //for monthly Interest Rate
+        double monthlyInterest = annualRate / MONTHS_IN_YEARS / PERCENT;
+
+        //No of payment
+        double numberOfPayment = year * MONTHS_IN_YEARS;
+
+        //for monthly payment
+        /*          M = P * r(1+r)^n / [(1+r)^n - 1]      */
+        //for (1+r)^n
+        double mortgage = principal
+                * monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayment)
+                / (Math.pow(1 + monthlyInterest, numberOfPayment)-1);
+
+        return mortgage;
     }
 }
